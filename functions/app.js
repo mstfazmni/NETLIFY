ad466c9a81c8b8d33f6cbf32a4866755b91280f1
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2024-04-07 00:26:04 -0600

    add register

--- functions/app.js
+++ functions/app.js
@@ -7,5 +7,36 @@ router.get("/", (req, res) => {
     res.send("App is running..");
 });
 
+
+app.post('/register', (req, res) => {
+    const { firstName, lastName, userId, address, status } = req.body;
+    
+    let fee;
+    switch (status) {
+        case 'student':
+            fee = 10;
+            break;
+        case 'staff':
+            fee = 50;
+            break;
+        case 'volunteer':
+            fee = 0;
+            break;
+        default:
+            fee = 0;
+    }
+    
+    const response = {
+        firstName: firstName,
+        lastName: lastName,
+        userId: userId,
+        address: address,
+        status: status,
+        fee: fee
+    };
+    
+    res.json(response);
+});
+
 app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
